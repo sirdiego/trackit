240b727ffaf89ebade5fc38d36f0e350b5e3e08c
diego <diego@sirdiego.de>
2015-02-20 22:22:31 +0100

    added .gitignore with new vendor directory and composer.lock file also updated index.php to use slim now

--- web/index.php
+++ web/index.php
@@ -1,2 +1,11 @@
 <?php
-echo "Hello trackit!";
+
+use Slim\Slim;
+
+require_once '../vendor/autoload.php';
+
+$app = new Slim();
+$app->get('/', function() {
+	echo "Hello trackit!";
+});
+$app->run();
